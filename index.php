2623be8e59460b1353b79da4f50ac798133cf9ca
Rodgrad <47482408+Rodgrad@users.noreply.github.com>
2020-08-28 00:11:12 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<link rel='stylesheet' href='base.css'/>
+</head>
+<header>
+    <h1>Nutricionet</h1>
+    <h3>Check food calories.</h3>
+</header>
+
+<body>
+<br><br>
+
+<div id='items-wrapper'>
+    
+    <form action="" id='form-item' method='GET'>
+        <input type="text" name='item'>
+        <button type="submit" id='form-btn'>Search/Trazi</button>
+    </form>
+    <div id='items'>
+        <h1>Type something to eat.</h1>
+    </div>
+</div>
+
+<script src="api_jquery.js"></script>
+
+</body>
+
+
+</html>
